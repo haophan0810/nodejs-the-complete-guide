ace1d680ba334207c09a8d3be5abb1f624fe3b4e
Huu Hao Phan <huuhaotk0810@gmail.com>
2018-11-20 05:49:55 +0700

    redirecting

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
 const http = require('http');
+const fs = require('fs');
 
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'Haophan');
-    res.write(`
+    
+    const url = req.url;
+    console.log('url :', url);
+    const method = req.method;
+    if(url === '/'){
+        res.write(`
+            <html>
+                <head>
+                </head>
+                <body>
+                    <form action='/message' name='message' method='POST' >
+                        <input type='text'></input>
+                        <button>Send</button>
+                    </form>
+                </body>
+            </html>
+        `);
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST' ){
+        fs.writeFileSync('output.txt', 'YES');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        res.write(`
         <html>
             <head>
-            
             </head>
             <body>
-            <h1>heloo</h1>
-            
-            
+              ok!
             </body>
         </html>
-    `);
+        `);
+    return res.end()
+    }
+
+    res.setHeader('Content_type', 'message');
+    res.write(`
+    <html>
+        <head>
+        </head>
+        <body>
+            <form action='/message' name='message' method='POST' >
+                <input type='text'></input>
+                <button>Send</button>
+            </form>
+        </body>
+    </html>
+`);
     res.end()
     
 
