2f713633adf5853acc165112e67664c82c27ea99
Huu Hao Phan <huuhaotk0810@gmail.com>
2018-11-20 05:13:07 +0700

    response

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ const http = require('http');
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log('res :', req);
+    res.setHeader('Content-Type', 'Haophan');
+    res.write(`
+        <html>
+            <head>
+            
+            </head>
+            <body>
+            <h1>heloo</h1>
+            
+            
+            </body>
+        </html>
+    `);
+    res.end()
     
+
 })
 
 server.listen(port);
